3fa03ea9d7d1ff1482215f420f659d1a921046ce
3isenHeiM <pierre@vivegnis.be>
2020-02-05 17:21:04 +0100

    Add feature to merge into exiting CherryTree file (#2)
    * Added merge feature to merge into existing document
    
    * Fixed test to check if dest_file was present
    
    * Updated Readme with new example
    
    * Added shebang for running the script from /home/pierre/perl5/bin:/home/pierre/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/pierre/Documents/virral/bin in linux
    
    * Updated for use with python3
    
    * Added comment about merging option. Only working with XML CherryTree files, not MySQL
    
    * Fixed issue while merging files
    
    * Refined the way to compute the biggest UID
    
    * Switched to lxml to have full XPath syntax available to search for biggest unique_id (no matter its place in the tree)

--- cherrymap.py
+++ cherrymap.py
@@ -1,4 +1,6 @@
-import xml.etree.cElementTree as ET
+#!/usr/bin/env python3
+
+from lxml import etree as ET
 from libnmap.parser import NmapParser
 import os
 import argparse
@@ -10,53 +12,78 @@ parser.add_argument("-ap", "--allports", action="store_true",
                     help="add ports closed or filtered")
 parser.add_argument("-a", "--all", action="store_true",
                     help="same as '-ah -ap'")
-parser.add_argument("folder",
-                    help="folder where nmap outputs are stored")
+parser.add_argument("-m", "--merge", action="store",
+                    help="Merge the content into another CherryTree file. That file must be closed.")
+parser.add_argument("file",
+                    help="Nmap XML file to parse (relative path to current directory)")
 args = parser.parse_args()
 
-path=args.folder+"/"
+# Absolute path of the file
+filename=os.path.abspath(args.file)
+
+dest_file = args.merge
+
 uid=1
 root = ET.Element("cherrytree")
 
-for filename in os.listdir(path):
-	if not filename.endswith('.xml'): continue
-	try:
-		rep = NmapParser.parse_fromfile(path+filename)
-	except:
-		continue
+try:
+    rep = NmapParser.parse_fromfile(filename)
+except:
+    print("Error while parsing file " + filename)
 
-	node = ET.SubElement(root, "node", custom_icon_id="0", foreground="", is_bold="False", name=filename.split(".")[0], prog_lang="custom-colors", readonly="False", tags="", unique_id=str(uid))
-	uid=uid+1
+if dest_file is None :
+    node = ET.SubElement(root, "node", custom_icon_id="0", foreground="", is_bold="False", name=os.path.basename(filename), prog_lang="custom-colors", readonly="False", tags="", unique_id=str(uid))
+    uid=uid+1
+else :
+    # Parse destination file
+    dest_tree = ET.parse(dest_file)
+    dest_tree_root = dest_tree.getroot()
+    
+    # Find the biggest value of unique_id to properly set it on new items
+    biggest_UID=0
+    for unique_id in dest_tree_root.xpath('//@unique_id') :
+       if int(unique_id) > biggest_UID:
+          biggest_UID = int(unique_id)
+    uid=biggest_UID+1
+    
+    # Set the node as SubElement of the destionation file XML tree
+    node = ET.SubElement(dest_tree_root, "node", custom_icon_id="0", foreground="", is_bold="False", name=os.path.basename(filename), prog_lang="custom-colors", readonly="False", tags="", unique_id=str(uid))
 
-	try:
-		with open(path+filename.split(".")[0]+".nmap") as f: s = f.read()
-		ET.SubElement(node, "rich_text").text=s
-	except EnvironmentError:
-		print "Nmap file not found it won't be added"
+try:
+    # Locate the .nmap file to parse it as the Tree Node content
+    with open(filename.split(".")[0]+".nmap") as f: summary = f.read()
+    ET.SubElement(node, "rich_text").text=summary
+except EnvironmentError:
+    print("Nmap file not found, the whole nmap output won't be added to document tree")
 
-	for _host in rep.hosts:
-		if (_host.is_up() and len(_host.services)>0) or args.allhosts or args.all:
-			host = ET.SubElement(node, "node", foreground="", is_bold="False", name=_host.address, prog_lang="custom-colors", readonly="False", tags="", unique_id=str(uid))
-			uid=uid+1
-			fing = ET.SubElement(host, "rich_text")
-			fp = str(_host.hostnames)+_host.os_fingerprint+"\n"
-			if _host.os_fingerprinted:
-				for os in _host.os_match_probabilities():
-					fp = fp + os.name + "\n"
-			fing.text=fp
-			for  _service in _host.services:
-				if _service.open() or args.allports or args.all:
-					color=""
-					if not _service.open():
-						color="#ff0000"
-					service = ET.SubElement(host, "node", foreground=color, is_bold="False", name=str(_service.port) + "/" + _service.protocol + " - " + _service.service, prog_lang="custom-colors", readonly="False", tags="", unique_id=str(uid))
-					uid=uid+1
-					ET.SubElement(service, "rich_text", style="italic", weight="heavy").text="Banner:\n"
-					ET.SubElement(service, "rich_text").text=_service.banner+"\n\n\n"
-					ET.SubElement(service, "rich_text", style="italic", weight="heavy").text="Scripts:\n"
-					for scr in _service.scripts_results:
-						ET.SubElement(service, "rich_text", weight="heavy").text=scr['id']+"\n"
-						ET.SubElement(service, "rich_text").text=scr['output']+"\n"
+for _host in rep.hosts:
+    if (_host.is_up() and len(_host.services)>0) or args.allhosts or args.all:
+        host = ET.SubElement(node, "node", foreground="", is_bold="False", name=_host.address, prog_lang="custom-colors", readonly="False", tags="", unique_id=str(uid))
+        uid=uid+1
+        fing = ET.SubElement(host, "rich_text")
+        fp = str(_host.hostnames)+_host.os_fingerprint+"\n"
+        if _host.os_fingerprinted:
+            for os in _host.os_match_probabilities():
+                fp = fp + os.name + "\n"
+        fing.text=fp
+        for  _service in _host.services:
+            if _service.open() or args.allports or args.all:
+                color=""
+                if not _service.open():
+                    color="#ff0000"
+                service = ET.SubElement(host, "node", foreground=color, is_bold="False", name=str(_service.port) + " (" + _service.protocol.upper() + ") - " + _service.service, prog_lang="custom-colors", readonly="False", tags="", unique_id=str(uid))
+                uid=uid+1
+                ET.SubElement(service, "rich_text", style="italic", weight="heavy").text="Banner:\n"
+                ET.SubElement(service, "rich_text").text=_service.banner+"\n\n\n"
+                ET.SubElement(service, "rich_text", style="italic", weight="heavy").text="Scripts:\n"
+                for scr in _service.scripts_results:
+                    ET.SubElement(service, "rich_text", weight="heavy").text=scr['id']+"\n"
+                    ET.SubElement(service, "rich_text").text=scr['output']+"\n"
 
-tree = ET.ElementTree(root)
-tree.write("cherrymap.ctd")
+if dest_file is None : 
+    tree = ET.ElementTree(root)
+    tree.write(os.path.splitext(filename)[0] + ".ctd")
+else :
+    # Overwrite the destination file with the new XML tree 
+    with open(dest_file, 'wb') as f:
+        dest_tree.write(f)
